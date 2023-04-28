59cf912d2c377bc1f5ba8602814575877b17a2dd
unknown <javascriptandreact6@gmail.com>
2023-04-29 00:52:46 +0600

    local environment file setup

--- src/firebse/firebase.config.js
+++ src/firebse/firebase.config.js
@@ -4,13 +4,14 @@ import { initializeApp } from "firebase/app";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+console.log('environment variable', import.meta.env.VITE_apiKey)
 const firebaseConfig = {
-  apiKey: "AIzaSyBImgXIujs-ajAyIIWrMiUkDT8SPvVxX40",
-  authDomain: "the-news-dragon-cb5a4.firebaseapp.com",
-  projectId: "the-news-dragon-cb5a4",
-  storageBucket: "the-news-dragon-cb5a4.appspot.com",
-  messagingSenderId: "793587970859",
-  appId: "1:793587970859:web:a28bcf7ccd389fa8018b6e"
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId,
 };
 
 // Initialize Firebase
